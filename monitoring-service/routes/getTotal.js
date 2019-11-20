fd085698ede64739fcf27358e3bbb4d9ab37e7ba
Dirkadin <dirkadin@gmail.com>
2019-11-19 20:06:59 -0600

    adding total math

--- monitoring-service/routes/getTotal.js
+++ monitoring-service/routes/getTotal.js
@@ -8,14 +8,20 @@ const log = require('simple-node-logger').createSimpleLogger('logs/events.log');
 router.get('/', function(req, res, next) {
     //reports back the total amount of earnings seen thus far.
 
+    let prices = [['hotdog', 20], ['hamburger', 35], ['soda', 4], ['cookie', 6]];
     let line;
+    var total = 0;
 
     while (line = liner.next()) {
-        //do stuff
+        var str = line.split(' ');
+        var item = str[2];
+        var quantity = str[3];
+
+        total += prices.get(item) * quantity;
     }
 
 
-    res.render('test', { title: 'Get Total', message:'' });
+    res.render('test', { title: 'Get Total', message:total });
 });
 
 module.exports = router;
