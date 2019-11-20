67dd831db99a4dadd6e75b434ac63cdd5506c566
Dirkadin <dirkadin@gmail.com>
2019-11-19 18:30:19 -0600

    testing routes

--- monitoring-service/routes/getTotal.js
+++ monitoring-service/routes/getTotal.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const lineByLine = require('n-readlines');
+const liner = new lineByLine('logs/orders.log');
+const log = require('simple-node-logger').createSimpleLogger('logs/events.log');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     //reports back the total amount of earnings seen thus far.
+
+    let line;
+
+    while (line = liner.next()) {
+        //do stuff
+    }
+
+
     res.render('test', { title: 'Get Total', message:'' });
 });
 
