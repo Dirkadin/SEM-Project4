fe383e1eb24d0e1efa7d0a459c560ebe215d35ad
Dirkadin <dirkadin@gmail.com>
2019-11-23 10:26:49 -0600

    added getlastrequesttime

--- monitoring-service/routes/getLastRequestTime.js
+++ monitoring-service/routes/getLastRequestTime.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const log = require('simple-node-logger').createSimpleLogger('logs/events.log');
+const fs = require('fs');
 
-/* GET home page. */
-router.get('/getlastrequesttime', function(req, res, next) {
-    //reports back when the last request occurred
-    res.render('test', { title: 'Get Last Request Time', message:'' });
+router.get('/', function(req, res, next) {
+    log.info('GET /getlastrequesttime');
+
+    var data = fs.readFileSync('./logs/events.log');
+    var content = data.toString();
+    var events = content.split('\n');
+    
+    //The logger adds a new line to the end of each log, so we wrap around twice
+    var latestEvent = events.slice(-2);
+
+    res.render('test', { title: 'The last requst was made: ' + latestEvent, message:'' });
 });
 
 module.exports = router;
